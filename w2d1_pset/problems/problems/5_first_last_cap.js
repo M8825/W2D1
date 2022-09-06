4545c1f9c8a907636f28f3941b552705c983155f
Mlkz <maho.mamulashvili@gmail.com>
2022-09-06 19:29:56 -0400

    first and last char

--- w2d1_pset/problems/problems/5_first_last_cap.js
+++ w2d1_pset/problems/problems/5_first_last_cap.js
@@ -5,13 +5,29 @@ uppercase. All other characters should be lowercase.
 
 Examples:
 
-firstLastCap('hello BOOTCAMP PrEp'); // =>'HellO BootcamP PreP'
+firstlastcap('hello bootcamp prep'); // =>'hello bootcamp prep'
 firstLastCap('what is on the radio'); // =>'WhaT IS ON ThE RadiO'
 ***********************************************************************/
 
 function firstLastCap(sentence) {
+  let arr_sent = sentence.split(' ');
+  let result = [];
 
+  for (let i = 0; i < arr_sent.length; i++){
+    let arr_curr_word = arr_sent[i].split('');
+
+    let first = arr_curr_word.shift().toUpperCase();
+    let last = arr_curr_word.pop().toUpperCase();
+    let curr_join = arr_curr_word.join('');
+    let word = first + curr_join.toLowerCase() + last;
+
+    result.push(word);
+  }
+
+  return result.join(' ');
 }
+
+console.log(firstLastCap('hello bootcamp prep'));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = firstLastCap;
