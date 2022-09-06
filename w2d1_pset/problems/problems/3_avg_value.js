0346dcd51646782d4d4449a68b228603bf91b134
Mlkz <maho.mamulashvili@gmail.com>
2022-09-06 18:47:59 -0400

    average of array

--- w2d1_pset/problems/problems/3_avg_value.js
+++ w2d1_pset/problems/problems/3_avg_value.js
@@ -10,7 +10,13 @@ avgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
 function avgValue(array) {
+  let sum = 0;
 
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum / array.length;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
